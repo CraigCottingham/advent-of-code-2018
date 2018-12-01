2c89acfa08e86cf7847b2bea24b0a9cb5b33a8a3
Craig S. Cottingham <craig@cottingham.net>
2018-12-01 17:18:35 -0600

    Add my favorite development and test dependencies

--- mix.exs
+++ mix.exs
@@ -4,9 +4,10 @@ defmodule AoC.MixProject do
   def project do
     [
       app: :advent_of_code,
-      version: "0.1.0",
+      version: "2018.0.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      preferred_cli_env: [espec: :test],
       deps: deps()
     ]
   end
@@ -21,8 +22,11 @@ defmodule AoC.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:credo, "~> 0.10.2", only: [:dev, :test], runtime: false},
+      {:espec, "~> 1.6", only: :test},
+      {:ex_machina, "~> 2.2"},
+      {:faker, "~> 0.11.0", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
     ]
   end
 end
