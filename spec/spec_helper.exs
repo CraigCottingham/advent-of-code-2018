732763859fb77d1fd4727fb924e638bb39ff4215
Craig S. Cottingham <craig@cottingham.net>
2018-12-03 12:44:20 -0600

    Move solution load to spec helper

--- spec/spec_helper.exs
+++ spec/spec_helper.exs
@@ -1,9 +1,26 @@
 ESpec.configure fn(config) ->
   config.before fn(tags) ->
-    {:shared, hello: :world, tags: tags}
+    {:shared, solutions: AoC.SpecHelper.load_solutions(), tags: tags}
   end
 
   config.finally fn(_shared) ->
     :ok
   end
+end
+
+defmodule AoC.SpecHelper do
+  @moduledoc false
+
+  def load_solutions do
+    :yamerl_constr.file("solutions.yml")
+    |> List.first
+    |> Enum.into(%{})
+    |> Enum.reduce(%{}, fn {k, v}, acc -> Map.put(acc, to_string(k), convert_values(v)) end)
+  end
+
+  defp convert_values(values) when is_list(values), do: Enum.map(values, &convert_value/1)
+  defp convert_values(values), do: values
+
+  defp convert_value(v) when is_list(v), do: to_string(v)
+  defp convert_value(v), do: v
 end
