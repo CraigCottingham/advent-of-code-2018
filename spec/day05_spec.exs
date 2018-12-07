276db98b384bc6e12a66e186afc62e6d7b6b3269
Craig S. Cottingham <craig@cottingham.net>
2018-12-06 22:56:41 -0600

    Add day 5 solution and specs

--- spec/day05_spec.exs
+++ spec/day05_spec.exs
@@ -4,11 +4,11 @@ defmodule AoC.Day05.Spec do
   use ESpec
 
   describe "sanity checks" do
-    before do: allow File |> to(accept(:stream!, fn(_, _, _) -> test_data() end))
+    before do: allow File |> to(accept(:stream!, fn(_, _, _) -> test_data() |> String.split(~r//) end))
     let :test_data, do: "dabAcCaCBAcCcaDA"
 
     it do
-      expect(AoC.Day05.part_1()) |> to(eq(0))
+      expect(AoC.Day05.part_1()) |> to(eq(10))
       expect(AoC.Day05.part_2()) |> to(eq(0))
     end
   end
